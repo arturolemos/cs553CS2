ae3dce3085a177a95570b8ffdc1080b5f1a288c9
Jose Manuel Perez <josemanuel2000@gmail.com>
2024-10-06 18:47:51 -0400

    Update securekeys.sh working perfectly

--- securekeys.sh
+++ securekeys.sh
@@ -1,22 +1,58 @@
 #!/bin/bash
 
-# Needs to be run from the same folder where student-admin_key is
-# Need to set executable permisions accordingly: chmod u+x securekeys.sh
+PORT=22015
+REMOTE_USER="student-admin"
+REMOTE_HOST="paffenroth-23.dyn.wpi.edu"
+AUTHORIZED_KEYS_PATH="~/.ssh/authorized_keys"
+PRIVATE_KEY_FILE="student-admin_key.pem"  # Path to the private key file
 
-port=22015  
+# Save the private key to a file
+cat <<EOF > "$PRIVATE_KEY_FILE"
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACAACRjdkS5FFxssF234a3fpZuOrXaEn1A4rLSxUNy+vbQAAAKAnwtEgJ8LR
+IAAAAAtzc2gtZWQyNTUxOQAAACAACRjdkS5FFxssF234a3fpZuOrXaEn1A4rLSxUNy+vbQ
+AAAEBUA21ze68DxYwSY9Fn+Tv8nDJ+sG4CzyfXy/QbRuqZmQAJGN2RLkUXGywXbfhrd+lm
+46tdoSfUDistLFQ3L69tAAAAGnJjcGFmZmVucm90aEBwYWZmZW5yb3RoLTIzAQID
+-----END OPENSSH PRIVATE KEY-----
+EOF
+chmod 600 "$PRIVATE_KEY_FILE"  # Ensure the private key has secure permissions
 
-if [ -z "$1" ]; then
-    read -p "Please enter the name of the key: " key_name
-else
-    key_name="$1"
-fi
+# Pre-defined public key contents
+ARTURO_KEY_PUBLIC="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICuil5VU/4F1yXT6SB8mM14imCrgKgcHHJEXLq+qwm2K arturo@LAPTOP-U3RU9JPS"
+JOSE_KEY_PUBLIC="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH/ximgjSS/j8aBYTKDOeHuYo5g919t1McNyLg8/b+3G josemanuel2000gmail.com@jose.local"
+DAVID_KEY_PUBLIC="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHpVLzBowx4E2euLHw93BMASzwmh9D70EInSXAREntiL david@Jennifer"
 
-if [ -z "$2" ]; then
-    read -p "Please enter the key password: " key_password
-else
-    key_password="$2"
-fi
+# Key to be removed (from your instruction)
+CLASS_KEY_PUBLIC="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAAJGN2RLkUXGywXbfhrd+lm46tdoSfUDistLFQ3L69t rcpaffenroth@paffenroth-23"
 
-ssh-keygen -t ed25519 -f "$key_name" -N "$key_password"
+# List of users and their keys
+users_keys=("$ARTURO_KEY_PUBLIC" "$JOSE_KEY_PUBLIC" "$DAVID_KEY_PUBLIC")
 
-scp -P "$port" -i student-admin_key "$key_name.pub" student-admin@paffenroth-23.dyn.wpi.edu:~/
+# Function to upload a key to the server
+upload_key() {
+    local user_key_content=$1
+
+    echo "Adding the public key to the server's authorized_keys..."
+    ssh -p "$PORT" -i "$PRIVATE_KEY_FILE" "$REMOTE_USER@$REMOTE_HOST" "echo '$user_key_content' >> $AUTHORIZED_KEYS_PATH"
+}
+
+# Function to remove the specific class key from the server
+remove_class_key() {
+    echo "Removing the class key from authorized_keys on the server..."
+    ssh -p "$PORT" -i "$PRIVATE_KEY_FILE" "$REMOTE_USER@$REMOTE_HOST" "sed -i '/$CLASS_KEY_PUBLIC/d' $AUTHORIZED_KEYS_PATH"
+}
+
+# Step 1: Add the public keys of the users to the server
+for key in "${users_keys[@]}"; do
+    echo "Processing key..."
+    upload_key "$key"
+done
+
+# Step 2: Remove the original class key from the server
+remove_class_key
+
+# Remove the local private key file
+rm -f "$PRIVATE_KEY_FILE"
+
+echo "Process complete: All keys updated on the server."
