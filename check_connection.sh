c2c78c3b604e4a486dc9020a03841f4d4fc08d02
Jose Manuel Perez <josemanuel2000@gmail.com>
2024-10-07 19:30:20 -0400

    Create check_connection.sh

---
+++ check_connection.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+REMOTE_USER="student-admin"
+REMOTE_HOST="paffenroth-23.dyn.wpi.edu"
+PRIVATE_KEY="./class_key"  
+PORT=22015 
+
+SCRIPTS_TO_RUN=("./secure_keys.sh" "./deploy.sh")
+
+ssh -i "$PRIVATE_KEY" -p "$PORT" -o BatchMode=yes -o ConnectTimeout=10 "$REMOTE_USER@$REMOTE_HOST" "exit"
+
+if [ $? -eq 0 ]; then
+    echo "Server is reachable via SSH. Running the scripts..."
+    for script in "${SCRIPTS_TO_RUN[@]}"; do
+        if [ -x "$script" ]; then
+            bash "$script"
+            echo "Executed $script"
+        else
+            echo "Script $script is not executable or not found"
+        fi
+    done
+else
+    echo "Cannot SSH to the server. Ignoring..."
+fi
